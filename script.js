e29481fa28b7966b84c62013a406746f158fb37e
Satyam <nikamsatyam187@gmail.com>
2025-11-13 17:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
-  // write your code here
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    
+    for (let i = 0; i < l; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters[randomIndex];
+    }
+    
+    return result;
 }
 
-// Do not change the code below.
+// Make function available globally if needed
+window.makeid = makeid;Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
